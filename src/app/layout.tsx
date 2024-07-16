11261da29a4cfb581c034bbf0a1b0e0b42269c2e
transcendence12 <gosiek.krawczuk@gmail.com>
2024-07-16 11:42:36 +0200

    fix: architecture, models, data, prisma, supabase (#2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '@/app/ui/global.css'
+import '@/src/components/global.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
