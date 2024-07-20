fe8f63ae1cb75148e264067b7cc2923a7fc8743a
transcendence12 <gosiek.krawczuk@gmail.com>
2024-07-20 18:47:31 +0200

    feat: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <p>Hello from main</p>
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5 leading-10">Witaj i znajdż wsparcie w przezwyciężaniu narcystycznej przemocy</h1>
+      <p className="max-w-[750px] mx-auto leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laudantium aliquid at non nulla assumenda corrupti soluta doloremque, est, adipisci tempora nostrum, unde praesentium natus dolorem consequatur ipsa quam quia!</p>
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-auto text-center text-zinc-400 py-5 px-7 border-t">
+      <small>
+        &copy; 2024. All rights reserved. Pomysł i wykonanie: Małgorzata
+        Krawczuk
+      </small>
+    </footer>
+  );
+};
