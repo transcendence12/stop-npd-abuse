5cf9fb72cd831a94d2b75189ff5144b5f9e34116
transcendence12 <gosiek.krawczuk@gmail.com>
2024-07-21 12:04:08 +0200

    chore: install and setup supabase ssr, client, server, middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
     images: {
         remotePatterns: [{
             protocol: "https",
