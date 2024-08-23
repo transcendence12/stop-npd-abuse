381a05b51ff1eef3daffdd8d7d84c6d2d216b75a
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-23 13:26:18 +0200

    fix: add link redirecting to social media, phone, mail, website

--- src/app/specialists/[id]/page.tsx
+++ src/app/specialists/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getSpecialistById } from "@/actions/getSpecialistById";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface SpecialistByIdProps {
@@ -20,37 +21,61 @@ async function SpecialistItemPage({ params }: SpecialistByIdProps) {
           specialist.specialisation.join(", ").replace("_", " ").toLowerCase()}
       </p>
       <p>
-        <strong>Miasto:</strong>{specialist.city}
+        <strong>Miasto:</strong>
+        {specialist.city}
       </p>
       <p>
-        <strong>Email:</strong>{specialist.email}
+        <strong>Email:</strong><a href={`mailto:${specialist.email}`}>{specialist.email}</a>
       </p>
       <p>
-        <strong>Numer telefonu:</strong>{specialist.phoneNumber}
+        <strong>Numer telefonu:</strong><a href={`tel:${specialist.phoneNumber}`}>{specialist.phoneNumber}</a>
+        
+      </p>
+      {specialist.website && (
+        <p>
+          <strong>Strona internetowa:</strong>
+          <Link href={specialist.website}>{specialist.website}</Link>
+        </p>
+      )}
+      <p>
+        <strong>WhatsApp:</strong>
+        {specialist.isWhatsApp ? "Tak" : "Nie"}
       </p>
       <p>
-        <strong>Strona internetowa:</strong>{specialist.website}
+        <strong>Online:</strong>
+        {specialist.isOnline ? "Tak" : "Nie"}
       </p>
       <p>
-        <strong>WhatsApp:</strong>{specialist.isWhatsApp ? "Tak" : "Nie"}
+        <strong>Na NFZ:</strong>
+        {specialist.isAcceptedNfz ? "Tak" : "Nie"}
       </p>
       <p>
-        <strong>Online:</strong>{specialist.isOnline ? "Tak" : "Nie"}
+        <strong>Płatnie:</strong>
+        {specialist.isPaid ? "Tak" : "Nie"}
       </p>
       <p>
-        <strong>Na NFZ:</strong>{specialist.isAcceptedNfz ? "Tak" : "Nie"}
+        <strong>Nazwa firmy:</strong>
+        {specialist.company || "Brak danych"}
       </p>
+      <div>
+        {specialist.socialMediaLinks && specialist.socialMediaLinks.length > 0 ? (
+          <div>
+          <strong>Media społecznościowe:</strong>
+          {
+            specialist.socialMediaLinks.map((link, index)=>(
+              <span key={index}>
+              <Link href={link.url}>{link.platform || link.url}</Link>
+              {specialist.socialMediaLinks && index < specialist.socialMediaLinks.length - 1 ? ", " : "" }
+              </span>
+            ))
+          }
+        </div>
+        ) : "Brak danych"}
+
+      </div>
       <p>
-        <strong>Płatnie:</strong>{specialist.isPaid ? "Tak" : "Nie"}
-      </p>
-      <p>
-        <strong>Nazwa firmy:</strong>{specialist.company || "N/A" }
-      </p>
-      <p>
-        <strong>Media społecznościowe:</strong>{specialist.socialMediaLinks?.map(link=> link.url)}
-      </p>
-      <p>
-        <strong>Książki:</strong>{specialist.books?.map(book => book.title)}
+        <strong>Książki:</strong>
+        {specialist.books?.map((book) => book.title + ", ")}
       </p>
     </main>
   );
