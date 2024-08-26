281fd7b317d43ee1d457bed8e917e248f1c63b3f
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-26 09:40:04 +0200

    feat: redirect from favorites specialist item to id page

--- src/components/FavoriteSpecialistsList.tsx
+++ src/components/FavoriteSpecialistsList.tsx
@@ -2,8 +2,9 @@
 import { getFavoriteSpecialist } from "@/actions/getFavoriteSpecialist";
 import { useEffect, useState } from "react";
 import { FavoriteSpecialist } from "@/types/FavoriteSpecialist";
+import Link from "next/link";
 
-export const FavoriteSpecialistsList:React.FC = () => {
+export const FavoriteSpecialistsList: React.FC = () => {
   const [favorites, setFavorites] = useState<FavoriteSpecialist[]>([]);
   useEffect(() => {
     const fetchFavorites = async () => {
@@ -14,15 +15,16 @@ export const FavoriteSpecialistsList:React.FC = () => {
     fetchFavorites();
   }, []);
   return (
-    <ul>
-      {favorites.map(
-        (specialist =>(
-          <li key={specialist.id}>
+    <ul className="w-4/5 text-center m-auto">
+      {favorites.map((specialist) => (
+        <Link href={`/specialists/${specialist.id}`} key={specialist.id}>
+          <li className="mb-4 border-b bg-sky-100 hover:bg-sky-300 hover:underline">
             {specialist.firstName} {specialist.lastName} -{" "}
-            {specialist.specialisationTypes && specialist.specialisationTypes.join(", ").replace(/_/g, " ")}
+            {specialist.specialisationTypes &&
+              specialist.specialisationTypes.join(", ").replace(/_/g, " ")}
           </li>
-        ))
-      )}
+        </Link>
+      ))}
     </ul>
   );
 };
