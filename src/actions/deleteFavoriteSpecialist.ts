c2321f8d7da62e1c4526f8a5dd77279aaaf3aadc
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-29 22:45:09 +0200

    fix: fixing bug in server action delete feature

--- src/actions/deleteFavoriteSpecialist.ts
+++ src/actions/deleteFavoriteSpecialist.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prismaClient";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export const deleteFavoriteSpecialist = async (specialistId: string) => {
   const userId = auth();
@@ -22,21 +23,35 @@ export const deleteFavoriteSpecialist = async (specialistId: string) => {
     if (!isUserExist) {
       throw new Error("Użytkownik nie istnieje. Zaloguj się.");
     }
-    if (userId) {
-      await prisma.favorite.delete({
-        
-          where: {
-            userId_specialistId: {
-                userId: user!.id,
-                specialistId: specialistId,
-            }
-            
-          },
-      });
+
+    // sprawdzenie czy rekord istnieje:
+    const checkFavorite = await prisma.favorite.findUnique({
+      where: {
+        userId_specialistId: {
+          userId: user.id,
+          specialistId: specialistId,
+        },
+      },
+    });
+    if (!checkFavorite) {
+      console.log(`Rekord z userId: ${user.id} i specialistId: ${specialistId} nie istnieje.`)
+      throw new Error("Nie można znaleźć rekordu do usunięcia.");
     }
+
+    await prisma.favorite.delete({
+      where: {
+        userId_specialistId: {
+          userId: user!.id,
+          specialistId: specialistId,
+        },
+      },
+    });
+    console.log(`USUNIĘTO!!!!! Rekord z userId: ${user.id} i specialistId: ${specialistId}`);
+    revalidatePath(`/favorites`)
   } catch (error) {
     console.error(
-      "Błąd podczas usuwania specialisty z listy ulubionych specjalistów.", error
+      "Błąd podczas usuwania specialisty z listy ulubionych specjalistów.",
+      error
     );
     throw new Error(
       "Nie udało się usunąć specialisty z listy ulubionych specjalistów. Spróbuj ponownie później."
