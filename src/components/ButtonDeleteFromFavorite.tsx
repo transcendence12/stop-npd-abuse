9494a36e21b10d5fabcd4bf631f4553c3b5fc0f1
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-29 21:37:10 +0200

    feat: add modal with delete confirmation for better UX

--- src/components/ButtonDeleteFromFavorite.tsx
+++ src/components/ButtonDeleteFromFavorite.tsx
@@ -13,18 +13,17 @@ interface ButtonDeleteFromFavoriteProps {
 export const ButtonDeleteFromFavorite: React.FC<
   ButtonDeleteFromFavoriteProps
 > = ({ specialistId, specialistName }) => {
-  const [isPending, startTransition] = useTransition();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
   const handleDelete = async () => {
     // const loggedInUser = await checkUser()
     try {
-      setError(null);
-      startTransition(async () => {
-        await deleteFavoriteSpecialist(specialistId);
-        router.refresh();
-      });
+      setIsPending(true);
+      router.refresh();
+      await deleteFavoriteSpecialist(specialistId);
     } catch (error) {
       console.error(
         "Nie udalo się usunąć specjalisty z Twojej listy ulubionych specjalistów.",
@@ -33,13 +32,46 @@ export const ButtonDeleteFromFavorite: React.FC<
       setError(
         "Nie udalo się usunąć specjalisty z Twojej listy ulubionych specjalistów. Spróbuj ponownie później."
       );
+    } finally {
+      setIsPending(false);
+      setIsModalOpen(false);
     }
   };
   return (
     <div>
-      <button type="button" className="hover:underline" onClick={handleDelete} disabled={isPending}>
-        {isPending ? "Usuwanie..." : "Usuń z ulubionych"}
+      <button
+        type="button"
+        className="hover:underline"
+        onClick={() => setIsModalOpen(true)}
+        disabled={isPending}
+      >
+        Usuń z ulubionych
       </button>
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-6 rounded shadow-md text-center">
+            <p className="mb-4">
+              Czy na pewno chcesz usunąć {specialistName} z listy ulubionych?
+            </p>
+
+            <button
+              className="bg-red-600 text-white px-4 py-2 rounded mr-2"
+              onClick={handleDelete}
+              disabled={isPending}
+            >
+              {isPending ? "Usuwanie..." : "Usuń"}
+            </button>
+            <button
+              className="bg-gray-300 px-4 py-2 rounded"
+              onClick={() => setIsModalOpen(false)}
+              disabled={isPending}
+            >
+              Anuluj
+            </button>
+          </div>
+        </div>
+      )}
+
       {error && <p className="text-red-500 mt-2">{error}</p>}
     </div>
   );
