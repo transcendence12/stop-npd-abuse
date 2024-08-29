8d13aa443451ce1aa1786be005beb6bccd6a8c50
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-29 10:43:23 +0200

    fix: error handling in likebtn component, redirect after sign in

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { incrementOrDecrementLike } from "@/actions/incrementOrDecrementLike";
-import { useUser } from "@clerk/nextjs";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export const LikeButton = ({
@@ -15,24 +15,35 @@ export const LikeButton = ({
 }) => {
   const [likes, setLikes] = useState(initialLikes);
   const [hasVoted, setHasVoted] = useState(hasJustLiked);
+  const [error, setError] = useState<string | null>(null);
   const { isSignedIn } = useUser();
+
+  const router = useRouter();
 
   const handleLikes = async () => {
     if (!isSignedIn) {
-      return <SignIn />;
+      router.push(
+        `/sign-in?redirect_url=${encodeURIComponent("/specialists")}`
+      );
+
+      // console.log("useerrrr", isSignedIn)
+
+      return;
     }
     try {
       const updatedLikes = await incrementOrDecrementLike(specialistId);
       setLikes(updatedLikes);
-      setHasVoted(!hasVoted)
+      setHasVoted(!hasVoted);
+      setError(null);
     } catch (error) {
       console.error("Błąd podczas aktualizacji polubień.", error);
+      setError("Wystąpił błąd podczas aktualizacji polubień");
     }
   };
   return (
     <div>
       <p>Suma polubień: {likes}</p>
-      {/* {error && <p className="text-red-500">{error}</p>} */}
+      {error && <p className="text-red-500">{error}</p>}
       <button onClick={handleLikes}>
         {hasVoted ? "Odlub 👎" : "Polub 👍"}
       </button>
