966b4a45b942eb559ca0b0f09f130049bbb3b89c
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-01 19:26:04 +0200

    feat: style list of all specialists

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -19,8 +19,12 @@ export const LikeButton = ({
   const { isSignedIn } = useUser();
 
   const router = useRouter();
+  const ariaLabel = hasVoted
+  ? `Odlub specjalistę. Liczba polubień: ${likes}`
+  : `Polub specjalistę. Liczba polubień: ${likes}`;
 
   const handleLikes = async () => {
+    
     if (!isSignedIn) {
       router.push(
         `/sign-in?redirect_url=${encodeURIComponent("/specialists")}`
@@ -31,6 +35,7 @@ export const LikeButton = ({
       return;
     }
     try {
+
       const updatedLikes = await incrementOrDecrementLike(specialistId);
       setLikes(updatedLikes);
       setHasVoted(!hasVoted);
@@ -41,11 +46,47 @@ export const LikeButton = ({
     }
   };
   return (
-    <div className="flex justify-start items-center gap-4">
-      <p>Suma polubień: {likes}</p>
+    <div className="flex justify-center items-center gap-4">
+      {/* <p>Suma polubień: {likes}</p> */}
       {error && <p className="text-red-500">{error}</p>}
-      <button className="btn" onClick={handleLikes}>
-        {hasVoted ? "Odlub 👎" : "Polub 👍"}
+      <button
+        className="btn btn-secondary"
+        onClick={handleLikes}
+        aria-label={ariaLabel}
+      >
+        {likes}
+        {/* {hasVoted ? "Odlub 👎" : "Polub 👍"} */}
+        {hasVoted ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="oklch(var(--sc))"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        )}
       </button>
     </div>
   );
