c7dd2baa5373f057c0f1905d54e3f6619127aff7
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-01 19:28:55 +0200

    feat: style list of all specialists (#17)

--- src/components/ButtonAddToFavorite.tsx
+++ src/components/ButtonAddToFavorite.tsx
@@ -16,6 +16,8 @@ export const ButtonAddToFavorite: React.FC<ButtonAddToFavoriteProps> = ({
   const [showLoginMessage, setShowLoginMessage] = useState(false);
   const router = useRouter();
 
+  const ariaLabel = "Zapisz specjaliste na liście ulubionych"
+
   const handleAddToFavorites = () => {
     if (!userId) {
       setShowLoginMessage(true);
@@ -33,13 +35,46 @@ export const ButtonAddToFavorite: React.FC<ButtonAddToFavoriteProps> = ({
     });
   };
   return (
-    <div>
+    <div className="flex justify-center items-center gap-4">
       <button
-        className="btn text-red-700"
+        className="btn"
         onClick={handleAddToFavorites}
         disabled={isPending}
+        aria-label={ariaLabel}
       >
-        {isPending ? "Dodawanie..." : "❤ Dodaj do ulubionych"}
+        {/* {isPending ? "Dodawanie..." : "❤ Zapisz w ulubionych"} */}
+        {isPending ? (
+          
+          <svg
+            className="h-6 w-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 13V7M9 10H15M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="h-6 w-6"
+            viewBox="0 0 24 24"
+            fill="oklch(var(--pc))"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 13V7M9 10H15M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        )}
       </button>
       {showLoginMessage && (
         <p className="text-red-500 mt-2">
--- src/components/ButtonSeeMore.tsx
+++ src/components/ButtonSeeMore.tsx
@@ -1,18 +1,37 @@
-"use client"
+"use client";
 
 import { Specialist } from "@/types/Specialist";
-import Link from "next/link"
+import Link from "next/link";
 
 interface ButtonSeeMoreProps {
-    specialistId: string;
-  }
-
-export const ButtonSeeMore:React.FC<ButtonSeeMoreProps> =({specialistId,}) => {
-  return (
-    <Link href={`specialists/${specialistId}`}>
-      <button type="button" className="btn mt-2 text-blue-600">
-        Zobacz więcej
-      </button>
-    </Link>
-  )
+  specialistId: string;
 }
+
+export const ButtonSeeMore: React.FC<ButtonSeeMoreProps> = ({
+  specialistId,
+}) => {
+  return (
+    <div className="flex justify-center items-center gap-4">
+      <Link href={`specialists/${specialistId}`}>
+        <button type="button" className="btn btn-primary">
+          Zobacz więcej
+          <svg
+            className="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+      </Link>
+    </div>
+  );
+};
