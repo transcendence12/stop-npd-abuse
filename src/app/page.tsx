613473d2f210fd6776b444516ef79b247f55cf7a
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-05 22:55:13 +0200

    feat: add click handler functionnality to the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,6 @@ export default function Home() {
   return (
     // className="text-center pt-32 px-5"
     <main>
-      {/* <h1 className="text-4xl md:text-5xl font-bold mb-12 leading-10">
-        Witaj i znajdź wsparcie w stawieniu czoła narcystycznej przemocy
-      </h1>
-      <p className="max-w-[750px] mx-auto leading-8">
-        Lista kontaktów do psychologów, psychoterapeutów, trenerów i prawników,
-        którzy udzielają wsparcia i pracują z osobami dotkniętymi specyficznym
-        rodzajem przemocy, tzw.przemocą narcystyczną. Znajdują się tutaj również
-        linki do mediów społecznościowych poszczególnych specjalistów, ich
-        strony internetowe, publikacje, a w miarę rozwoju aplikacji dołączę bazę wiedzy.
-      </p> */}
       <Hero />
     </main>
   );
--- src/components/ButtonHero.tsx
+++ src/components/ButtonHero.tsx
@@ -1,5 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export const ButtonHero = () => {
-    return (
-        <button type="button" className="btn btn-primary">Wyświetl specjalistów</button>
-    )
-}
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  const handleRedirectToSpecialistList = () => {
+    setLoading(true);
+    setErrorMessage(null)
+    try {
+      router.push("/specialists");
+    } catch(error) {
+      console.error("Błąd podczas przechodzenia do strony specjalistów", error);
+      setErrorMessage("Wystąpił problem podczas przekierowywania. Spróbuj ponownie.")
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      {loading ? (
+        <p>Ładowanie....</p>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleRedirectToSpecialistList}
+        >
+          Wyświetl specjalistów
+        </button>
+      )}
+      {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+    </>
+  );
+};
