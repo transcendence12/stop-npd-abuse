6368221d04e885302464a3478d131dfc4af3cda1
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-05 23:05:47 +0200

    feat: error alert dispalying to the user for better ux

--- src/components/ButtonHero.tsx
+++ src/components/ButtonHero.tsx
@@ -1,23 +1,33 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ButtonHero = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const handleRedirectToSpecialistList = () => {
     setLoading(true);
-    setErrorMessage(null)
+    setErrorMessage(null);
     try {
       router.push("/specialists");
-    } catch(error) {
+    } catch (error) {
       console.error("Błąd podczas przechodzenia do strony specjalistów", error);
-      setErrorMessage("Wystąpił problem podczas przekierowywania. Spróbuj ponownie.")
+      setErrorMessage(
+        "Wystąpił problem podczas przekierowywania. Spróbuj ponownie."
+      );
     } finally {
       setLoading(false);
     }
   };
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
   return (
     <>
       {loading ? (
@@ -31,7 +41,24 @@ export const ButtonHero = () => {
           Wyświetl specjalistów
         </button>
       )}
-      {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+      {errorMessage && (
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{errorMessage}.</span>
+        </div>
+      )}
     </>
   );
 };
