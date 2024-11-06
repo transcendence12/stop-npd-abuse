f7cca1cca49c55fa67f132b389e989fce27485fa
transcendence12 <gosiek.krawczuk@gmail.com>
2024-11-06 12:34:25 +0100

    feat: search specialist

--- src/components/SearchSpecialist.tsx
+++ src/components/SearchSpecialist.tsx
@@ -14,7 +14,7 @@ export const SearchSpecialist = () => {
 
   const handleSearch = useDebouncedCallback((query: string) => {
     startTransition(() => {
-    //   console.log("Searching...", query);
+      //   console.log("Searching...", query);
       const params = new URLSearchParams(searchParams);
       if (query) {
         params.set("query", query);
@@ -32,19 +32,18 @@ export const SearchSpecialist = () => {
       handleSearch("");
     }
   };
-//   useEffect(() => {
-//     if (inputRef.current) {
-//         inputRef.current.value = q || "";
-//     }
-// }, [q]);
+  //   useEffect(() => {
+  //     if (inputRef.current) {
+  //         inputRef.current.value = q || "";
+  //     }
+  // }, [q]);
 
   return (
     <section className="flex justify-center items-center">
-        <div className="join mb-8">
-      <div>
-        <div>
+      <div className="join mb-8">
+        <label className="input input-bordered join join-item relative flex items-center gap-2 pr-10">
           <input
-            className="input input-bordered join-item"
+            className="join-item grow"
             placeholder="Szukaj specjalisty"
             onChange={(e) => {
               handleSearch(e.target.value);
@@ -59,29 +58,31 @@ export const SearchSpecialist = () => {
           />
           {q && (
             <button
-              className="btn btn-ghost btn-circle btn-xs"
+              // className="btn btn-ghost btn-circle btn-xs"
+              className={`absolute right-2 text-gray-500 ${
+                q ? "visible" : "invisible"
+              }`}
               onClick={handleClearInput}
             >
               &#10005;
             </button>
           )}
+        </label>
+
+        <select className="select select-bordered join-item">
+          <option disabled selected>
+            Kategoria
+          </option>
+          <option>Psychologia</option>
+          <option>Prawo</option>
+          <option>Inne</option>
+        </select>
+        <div className="indicator">
+          <button className="btn btn-accent join-item">
+            {isSearching ? <span>Szukanie...</span> : <span>Szukaj</span>}
+          </button>
         </div>
       </div>
-      {/* <select className="select select-bordered join-item">
-        <option disabled selected>
-          Kategoria
-        </option>
-        <option>Psychologia</option>
-        <option>Prawo</option>
-        <option>Inne</option>
-      </select> */}
-      <div className="indicator">
-        <button className="btn btn-accent join-item">
-          {isSearching ? <span>Szukanie...</span> : <span>Szukaj</span>}
-        </button>
-      </div>
-    </div>
     </section>
-    
   );
 };
